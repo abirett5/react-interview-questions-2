a90eb25f68e426b574a83258743d8392ab4bbdc6
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-08-04 17:11:41 +0600

    Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./styles.css";
-import Folder from "./components/Folder";
-import explorer from "./data/folderData";
 
 export default function App() {
   return (
     <div className="App">
-      <Folder explorer={explorer} />
+      
     </div>
   );
 }
