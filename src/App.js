c6f1d21ad04da4931889cbf87b8ff4b2651f54f0
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-08-04 19:39:03 +0600

    Updated

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React from "react";
 import "./styles.css";
 
 class App extends React.component{
+  state = {
+    num: 0,
+  };
+
+
+  handleClick() {
+    this.setState((state) => ( {num: state.num + 1} ));
+  }
+
   render() {
-    return <button>Increment</button>;
+    return <><button onClick={this.handleClick.bind(this)}>Increment</button>
+    <Counter number={this.state.num} />
+    </>;
   }
 }
 
